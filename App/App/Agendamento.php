396a3a79545ce9b37c4cf640138e17166b21609c
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2020-05-06 18:01:51 -0300

    models

---
+++ App/App/Agendamento.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\App;
+
+use App\App\Base\Agendamento as BaseAgendamento;
+
+/**
+ * Skeleton subclass for representing a row from the 'agendamento' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class Agendamento extends BaseAgendamento
+{
+
+}
---
+++ App/App/Contrato.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\App;
+
+use App\App\Base\Contrato as BaseContrato;
+
+/**
+ * Skeleton subclass for representing a row from the 'contrato' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class Contrato extends BaseContrato
+{
+
+}
