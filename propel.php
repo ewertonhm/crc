1eec29d92ee55a5552dba356c58bb33731b5e36f
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2020-05-06 16:49:38 -0300

    propel fix2

--- propel.php
+++ propel.php
@@ -9,10 +9,30 @@ return [
                     'user' => 'postgres',
                     'password' => 'postgres',
                     'settings' => [
+                        'charset' => 'utf8',
+                        'queries' => [
+                            'utf8' => "SET NAMES 'UTF8'"
+                        ]
+                    ]
+                ],
+                'crc' => [
+                    'adapter' => 'pgsql',
+                    'dsn' => 'pgsql:host=localhost;port=5432;dbname=crc',
+                    'user' => 'postgres',
+                    'password' => 'postgres',
+                    'settings' => [
                         'charset' => 'utf8'
                     ]
                 ]
             ]
+        ],
+        'runtime' => [
+            'defaultConnection' => 'crc',
+            'connections' => ['crc']
+        ],
+        'generator' => [
+            'defaultConnection' => 'crc',
+            'connections' => ['crc']
         ]
     ]
 ];
