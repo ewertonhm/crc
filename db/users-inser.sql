c7a725d9f42e72371e349b7e025d39f41b0bf38f
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2020-05-13 16:02:41 -0300

    Update users-inser.sql

--- db/users-inser.sql
+++ db/users-inser.sql
@@ -1,1 +1,1 @@
-insert into atendente (nome,login,senha,permissao) values ('Ewerton','suporte.ewerton@toque.com.br','b8d7ea3e4bddae8de751ff289be2fd63','1')
+insert into atendente (nome,login,senha,permissao,lista,form) values ('Ewerton','suporte.ewerton@toque.com.br','b8d7ea3e4bddae8de751ff289be2fd63',1,0,0)
