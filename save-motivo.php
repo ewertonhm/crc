bce2ebe6ec67f9c6268f0f5e0270d861544c857f
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2020-05-19 10:40:35 -0300

    Create save-motivo.php

---
+++ save-motivo.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'config.php';
+if(!\controller\User::checkPermission(2)){
+    header('location:warning.php');
+}
+
+if(isset($_POST['action'])){
+    $u = new Motivo();
+    $u->setMotivo($_POST['form1']);
+    $u->save();
+}
+
+if(isset($_POST['edit'])){
+    $u = MotivoQuery::create()->findOneById((int)$_GET['id']);
+    if(isset($_POST['form2']) AND $_POST['form2'] != NULL AND $_POST['form2'] != ''){
+        $u->setMotivo($_POST['form2']);
+    }
+    $u->save();
+}
+
+if(isset($_POST['delete'])){
+    $u = MotivoQuery::create()->findOneById((int)$_GET['id']);
+    $u ->delete();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+header('location:lista-motivos.php');
+?>
