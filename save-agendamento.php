247229bc25ce50571679bf3392c3d94e36c0b242
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2020-05-19 10:40:40 -0300

    Create save-agendamento.php

---
+++ save-agendamento.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'config.php';
+if(!\controller\User::checkPermission(2)){
+    header('location:warning.php');
+}
+
+if(isset($_POST['action'])){
+    $u = new Agendamento();
+    $u->setAgendamento($_POST['form1']);
+    $u->save();
+}
+
+if(isset($_POST['edit'])){
+    $u = AgendamentoQuery::create()->findOneById((int)$_GET['id']);
+    if(isset($_POST['form2']) AND $_POST['form2'] != NULL AND $_POST['form2'] != ''){
+        $u->setAgendamento($_POST['form2']);
+    }
+    $u->save();
+}
+
+if(isset($_POST['delete'])){
+    $u = AgendamentoQuery::create()->findOneById((int)$_GET['id']);
+    $u ->delete();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+header('location:lista-agendamentos.php');
+?>
