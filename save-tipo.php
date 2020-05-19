58cceceaa68a9a668c162de052c59a5742f4725b
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2020-05-19 10:36:32 -0300

    Edição tipo de cliente

--- save-tipo.php
+++ save-tipo.php
@@ -12,17 +12,8 @@ if(isset($_POST['action'])){
 
 if(isset($_POST['edit'])){
     $u = TipoQuery::create()->findOneById((int)$_GET['id']);
-    if(isset($_POST['nome']) AND $_POST['nome'] != NULL AND $_POST['nome'] != ''){
-        $u->setNome($_POST['nome']);
-    }
-    if(isset($_POST['login']) AND $_POST['login'] != NULL AND $_POST['login'] != ''){
-        $u->setLogin($_POST['login']);
-    }
-    if(isset($_POST['senha']) AND $_POST['senha'] != NULL AND $_POST['senha'] != ''){
-        $u->setSenha(md5($_POST['senha']));
-    }
-    if(isset($_POST['permissao']) AND $_POST['permissao'] != NULL AND $_POST['permissao'] != ''){
-        $u->setPermissao((int)$_POST['permissao']);
+    if(isset($_POST['form2']) AND $_POST['form2'] != NULL AND $_POST['form2'] != ''){
+        $u->setTipo($_POST['form2']);
     }
     $u->save();
 }
