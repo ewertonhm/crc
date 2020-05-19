e5656667328209303d77c4f0dde72d2529f310e6
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2020-05-19 16:32:52 -0300

    Create del-contrato.php

---
+++ del-contrato.php
@@ -0,0 +1,69 @@
+<?php
+require_once 'config.php';
+if(!\controller\User::checkPermission(2)){
+    header('location:warning.php');
+}
+?>
+
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Materialize CSS -->
+    <link rel="stylesheet" href="css/materialize/materialize.min.css">
+    <link rel="stylesheet" href="css/materialize/form.css">
+
+    <title>Planilha do C.R.C - Deletar Contrato</title>
+</head>
+<body>
+
+<div class="row">
+    <form autocomplete="off" name="form1" class="col s12" action="save-contrato.php?id=<?php echo $_GET['id'];?>" method="post">
+        <div class="col s12">
+            <div class="row ">
+                <h3 class="green-text">Deletar Contrato</h3>
+            </div>
+            <div class="row">
+                <div class="input-field col s3">
+                    <input type="text" name="id" class="nome" value="<?php echo $_GET['id'];?>" disabled>
+                    <label for="autocomplete-input">ID</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s3">
+                    <input type="text" name="nome" disabled value="<?php echo ContratoQuery::create()->findOneById((int)$_GET['id'])->getContrato();?>" class="nome">
+                    <label for="autocomplete-input">Contrato</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s1">
+                    <button class="btn waves-effect waves-light green" type="submit" name="delete">Deletar
+                        <i class="material-icons right">send</i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+
+
+<!-- Compiled and minified JavaScript -->
+<script src="js/jquery-3.5.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+
+<script type='text/javascript'>
+    $(document).ready(function(){
+        $('select').formSelect();
+    });
+
+</script>
+</body>
+</html>
