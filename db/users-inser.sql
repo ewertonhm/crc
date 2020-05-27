149dc0aded4321c594bbb3b1fffa9cfd72d08d38
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2020-05-27 16:51:31 -0300

    dados db

--- db/users-inser.sql
+++ db/users-inser.sql
@@ -1,2 +1,11 @@
-insert into atendente (nome,login,senha,permissao,lista,form) values ('Ewerton','suporte.ewerton@toque.com.br','b8d7ea3e4bddae8de751ff289be2fd63',1,0,0);
-insert into atendente (nome,login,senha,permissao,lista,form) values ('Admin-ewerton','admin.ewerton@toque.com.br','b8d7ea3e4bddae8de751ff289be2fd63',2,0,0)
+insert into atendente (nome,login,senha,permissao,lista,form) values ('Ewerton','suporte.ewerton@toque.com.br','b8d7ea3e4bddae8de751ff289be2fd63',2,0,0);
+insert into atendente (nome,login,senha,permissao,lista,form) values ('Matheus','suporte.matheus@toque.com.br','e10adc3949ba59abbe56e057f20f883e',2,0,0);
+insert into atendente (nome,login,senha,permissao,lista,form) values ('Anderson Thiago','suporte.anderson@toque.com.br','e10adc3949ba59abbe56e057f20f883e',0,0,0);
+insert into atendente (nome,login,senha,permissao,lista,form) values ('Leonardo','suporte.leonardo@toque.com.br','e10adc3949ba59abbe56e057f20f883e',0,0,0);
+insert into atendente (nome,login,senha,permissao,lista,form) values ('Ricardo','suporte.ricardo@toque.com.br','e10adc3949ba59abbe56e057f20f883e',0,0,0);
+insert into atendente (nome,login,senha,permissao,lista,form) values ('Felipe','suporte.felipe@toque.com.br','e10adc3949ba59abbe56e057f20f883e',0,0,0);
+insert into atendente (nome,login,senha,permissao,lista,form) values ('Diego','suporte.diego@toque.com.br','e10adc3949ba59abbe56e057f20f883e',0,0,0);
+
+
+
+
